524aacc130dc2817259e1eb9a4bc08d2bdd1093f
Geoffrey Johnson <GeoffJ_1@hotmail.com>
2021-11-26 21:39:25 -0600

    additional work on game logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,25 +5,36 @@ var questionsAskedEl = document.getElementById("questions-asked");
 var secondsRemainingEl = document.getElementById("seconds-remaining");
 var gameTimeEl = document.getElementById("game-time");
 var gameTime = 120; //set time for game in seconds
-// pregame and game card sections
+// pregame elements
 var preGameEl = document.querySelector(".pregame");
 var startBtn = document.getElementById("start-btn");
 var quizOptions = document.querySelector(".quiz-options");
+// game-card elements
 var gameCardEl = document.querySelector(".game-card");
-
+var answerChoicesEl = document.querySelector(".answer-choices");
+// postgame element
+var postgameEl = document.querySelector(".postgame");
+// quizzes in json
+var quizzes;
 // game state
 var playingGame = false;
+var numCorrect = 0;
+var numAsked = 0;
 
 
 // initial ui set up and presentation of pregame to user with quiz options from quiz-data.json
 function init() {
+    // retrieve quiz names from JSON and set to li
+    // getQuizData(loadQuestionData);
+
+
     secondsRemainingEl.textContent = gameTime;
     gameTimeEl.textContent = gameTime;
 
-// TODO get quiz names from JSON and set to li
+
 }
 
-// et game state, hide pregame and display game-card, start timer
+// set game state, hide pregame and display game-card, start timer
 function startGame() {
     playingGame = true;
 
@@ -32,26 +43,25 @@ function startGame() {
 
     // start game timer
     fireGameTimer(gameTime);
+    loadQuestionData();
 }
 
 // start countdown timer and refresh display every 1000ms
-function fireGameTimer(time) {
-    var timer = time;
+function fireGameTimer() {
 
     var timeInterval = setInterval(function () {
-        secondsRemainingEl.textContent = timer;
-        console.log(timer)
+        secondsRemainingEl.textContent = gameTime;
 
-        if (timer > 0 && timer > 9) {
-            secondsRemainingEl.textContent = timer;
-            timer--;
-        } else if (timer <= 9 && timer > 0) {
+        if (gameTime > 0 && gameTime > 9) {
+            secondsRemainingEl.textContent = gameTime;
+            gameTime--;
+        } else if (gameTime <= 9 && gameTime > 0) {
             // set color when time runs out
             secondsRemainingEl.style.color = "red";
             secondsRemainingEl.style.fontSize = "2em";
 
-            secondsRemainingEl.textContent = timer;
-            timer--;
+            secondsRemainingEl.textContent = gameTime;
+            gameTime--;
         } else {
             clearInterval(timeInterval);
             gameOver();
@@ -59,17 +69,73 @@ function fireGameTimer(time) {
     }, 1000);    
 }
 
-function loadQuestionData() {
-    // GET QUESTION DATA AND DISPLAY TO USER
+// RETRIEVE QUIZZES FROM JSON
+async function getQuizData() {
+    const response = fetch("/assets/data/quiz-data.json")
+    .then(response => response.json()) 
+    .then(jsonObject => console.log(jsonObject));
+
+    console.log(response.json());
+    // {
+    //     if (response.ok) {
+    //         quizzes= response.json();
+    //         // return response.json();
+    //     }
+    //     return Promise.reject(response);
+    // }).catch(function (error) {
+    //     console.warn(error);
+    //     alert("Unable to load quizzes.");
+    // });
+
 }
 
-function checkAnswer() {
-// CHECK USER INPUT AGAINST SAVED ANSWER FOR QUESTION
+function loadQuestionData() {
+    var questionEl = document.querySelector(".question");
+
+    // GET QUESTION DATA AND DISPLAY TO USER   
+    // quizzes = JSON.parse(quizJson);
+    // console.log(quizzes);
+
+    var question = "What is the question that goes here?";
+    var choices = ["first choice", "second choice", "third choice", "fourth choice"];
+
+    //show question
+    questionEl.textContent = question;
+
+    // iterate choices and show on screen
+    for (choice in choices) {
+        answerChoicesEl.children[choice].textContent = choices[choice];
+        }
+    };
+
+function checkAnswer(userAnswer) {
+    console.log(userAnswer);
+    var answer = 1;
+    // CHECK USER INPUT AGAINST SAVED ANSWER FOR QUESTION
+    if (userAnswer === answer) {
+        // Display CORRECT MESSAGE AND UPDATE 
+        console.log("CORRECT");
+        numCorrect++;
+    } else {
+        // Display WRONG MESSAGE
+        console.log("WRONG");
+        // check time remaining; if at least 10 seconds, subtract from timer; otherwise end game.
+        if (gameTime > 10) {
+            gameTime -= 10; // subtract 10 seconds from clock
+        } else {
+            clearInterval(timeInterval);
+            gameOver();
+        }
+    }
+        // update questions asked
+        numAsked++;
 }
 
 function gameOver() {
     //DO STUFF HERE WHEN THE GAME IS DONE
     playingGame = false;
+    gameCardEl.style.display = "none";
+    postgameEl.style.display = "block";
     console.log("GAME OVER MAN");
 }
 
@@ -77,5 +143,228 @@ function gameOver() {
 init();
 
 // EVENT LISTENERS
+// game start btn clicked
 startBtn.addEventListener("click", startGame);
+// listener for answer choices clicked
+answerChoicesEl.addEventListener("click", checkAnswer(this.id));
 
+
+// // RETRIEVE QUIZZES FROM JSON
+// async function getQuizData() {
+//     const response = await fetch("/assets/data/quiz-data.json");
+//     quizzes= await response.json();
+//     // console.log(response.status)
+//     if (response.status == 200) {
+//         getQuizData()
+//     } else {
+//         alert("Unable to load quizzes.");
+//     }
+
+// }
+
+var quizJson = {
+    "JavaScript Quiz": {
+        "q1": {
+            "question": "Ask a question here?",
+            "choices": [
+                "first choice",
+                "second choice",
+                "third choice",
+                "fourth choice"
+            ],
+            "answer": "c"
+        },
+        "q2": {
+            "question": "Ask a question here?",
+            "choices": [
+                "first choice",
+                "second choice",
+                "third choice",
+                "fourth choice"
+            ],
+            "answer": "c"
+        },
+        "q3": {
+            "question": "Ask a question here?",
+            "choices": [
+                "first choice",
+                "second choice",
+                "third choice",
+                "fourth choice"
+            ],
+            "answer": "d"
+        },
+        "q4": {
+            "question": "Ask a question here?",
+            "choices": [
+                "first choice",
+                "second choice",
+                "third choice",
+                "fourth choice"
+            ],
+            "answer": "a"
+        },
+        "q5": {
+            "question": "Ask a question here?",
+            "choices": [
+                "first choice",
+                "second choice",
+                "third choice",
+                "fourth choice"
+            ],
+            "answer": "a"
+        },
+        "q6": {
+            "question": "Ask a question here?",
+            "choices": [
+                "first choice",
+                "second choice",
+                "third choice",
+                "fourth choice"
+            ],
+            "answer": "d"
+        },
+        "q7": {
+            "question": "Ask a question here?",
+            "choices": [
+                "first choice",
+                "second choice",
+                "third choice",
+                "fourth choice"
+            ],
+            "answer": "b"
+        },
+        "q8": {
+            "question": "Ask a question here?",
+            "choices": [
+                "first choice",
+                "second choice",
+                "third choice",
+                "fourth choice"
+            ],
+            "answer": "b"
+        },
+        "q9": {
+            "question": "Ask a question here?",
+            "choices": [
+                "first choice",
+                "second choice",
+                "third choice",
+                "fourth choice"
+            ],
+            "answer": "d"
+        },
+        "q10": {
+            "question": "Ask a question here?",
+            "choices": [
+                "first choice",
+                "second choice",
+                "third choice",
+                "fourth choice"
+            ],
+            "answer": "a"
+        }
+    },
+    "Cat Quiz": {
+        "q1": {
+            "question": "Ask a question here?",
+            "choices": [
+                "first choice",
+                "second choice",
+                "third choice",
+                "fourth choice"
+            ],
+            "answer": "c"
+        },
+        "q2": {
+            "question": "Ask a question here?",
+            "choices": [
+                "first choice",
+                "second choice",
+                "third choice",
+                "fourth choice"
+            ],
+            "answer": "c"
+        },
+        "q3": {
+            "question": "Ask a question here?",
+            "choices": [
+                "first choice",
+                "second choice",
+                "third choice",
+                "fourth choice"
+            ],
+            "answer": "d"
+        },
+        "q4": {
+            "question": "Ask a question here?",
+            "choices": [
+                "first choice",
+                "second choice",
+                "third choice",
+                "fourth choice"
+            ],
+            "answer": "a"
+        },
+        "q5": {
+            "question": "Ask a question here?",
+            "choices": [
+                "first choice",
+                "second choice",
+                "third choice",
+                "fourth choice"
+            ],
+            "answer": "a"
+        },
+        "q6": {
+            "question": "Ask a question here?",
+            "choices": [
+                "first choice",
+                "second choice",
+                "third choice",
+                "fourth choice"
+            ],
+            "answer": "d"
+        },
+        "q7": {
+            "question": "Ask a question here?",
+            "choices": [
+                "first choice",
+                "second choice",
+                "third choice",
+                "fourth choice"
+            ],
+            "answer": "b"
+        },
+        "q8": {
+            "question": "Ask a question here?",
+            "choices": [
+                "first choice",
+                "second choice",
+                "third choice",
+                "fourth choice"
+            ],
+            "answer": "b"
+        },
+        "q9": {
+            "question": "Ask a question here?",
+            "choices": [
+                "first choice",
+                "second choice",
+                "third choice",
+                "fourth choice"
+            ],
+            "answer": "d"
+        },
+        "q10": {
+            "question": "Ask a question here?",
+            "choices": [
+                "first choice",
+                "second choice",
+                "third choice",
+                "fourth choice"
+            ],
+            "answer": "a"
+        }
+    }
+};
