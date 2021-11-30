a88e9071ad836e3512de718ce27d261059eee235
Geoffrey Johnson <GeoffJ_1@hotmail.com>
2021-11-30 00:01:05 -0600

    fixed highscore page to correctly sort and display saved scores

--- assets/js/hs-script.js
+++ assets/js/hs-script.js
@@ -2,15 +2,47 @@ var scoreListEl = document.querySelector(".score-list");
 
 // check local storage for saved scores, display if exists
 function loadSavedScores() {
- var savedScores = localStorage.getItem(savedScores);
- if (savedScores) {
+    var savedScores = JSON.parse(localStorage.getItem("savedScores")) || [];
+
+    // sort scores
+        console.log(savedScores.sort(sortScores));
+
+
+        // for (score in savedScores) {
+        //     var output = savedScores[score]["initials"] + " - " + savedScores[score]["score"];
+
+        //     scoreArray.push(savedScores[score][1]);
+        // }
+        // if (scoreArray.sort((a, b) => a - b)[0] < newScore) {
+        //     return true;
+        // } else {
+        //     return false;
+        // };
+
+    // add elements and display scores
     for (score in savedScores) {
-        var output = savedScores[score];
-        console.log(output);
-        scoreListEl.children[score].textContent = output;
+        var output = savedScores[score]["initials"] + " - " + savedScores[score]["score"];
+        console.log(output)
+        var newLi = document.createElement("li");
+        newLi.textContent = output;
+        scoreListEl.appendChild(newLi);
     }
- }
 };
+
+function sortScores(a, b) {
+    const scoreA = a.score;
+    const scoreB = b.score;
+
+    let comparison = 0;
+    if (scoreA > scoreB) {
+        comparison = 1;
+    } else if (scoreA < scoreB) {
+        comparison = -1;
+    }
+    return comparison * -1;
+}
+
+loadSavedScores();
 
 // function to return to main page when Go Back to Game button pressed
 function goBack() {
